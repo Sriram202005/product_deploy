7120b8628c55fd524a01b50cab57ce059820ee14
Sriram <165491916+Sriram202005@users.noreply.github.com>
2025-02-03 14:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen(2555, ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb+srv://sriram202005:578b31053@mycluster.kmn9y.mongodb.net/?retryWrites=true&w=majority&appName=Mycluster')
+mongoose.connect('mongodb+srv://sriram202005:578b31053@mycluster.kmn9y.mongodb.net/')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
